9219771df85e5954ffa22f11602d39967356c7d6
gvicari <indirizzoemail@gmail.com>
2021-02-01 20:27:13 +0000

    updated module 2

--- Module02.py
+++ Module02.py
@@ -767,22 +767,83 @@ banking['acct_year'] = banking['account_opened'].dt.strftime('%Y')
 print(banking['acct_year'])
 
 # ---------------------------------------------------------
+# Store fund columns to sum against
+fund_columns = ['fund_A', 'fund_B', 'fund_C', 'fund_D']
 
+# Find rows where fund_columns row sum == inv_amount
+inv_equ = banking[fund_columns].sum(axis = 1) == banking['inv_amount']
+
+# Store consistent and inconsistent data
+consistent_inv = banking[inv_equ]
+inconsistent_inv = banking[~inv_equ]
+
+# Store consistent and inconsistent data
+print("Number of inconsistent investments: ", inconsistent_inv.shape[0])
 
 # ---------------------------------------------------------
+# Store today's date and find ages
+today = dt.date.today()
+ages_manual = today.year - banking['birth_date'].dt.year
 
+# Find rows where age column == ages_manual
+age_equ = ages_manual == banking['age']
+
+# Store consistent and inconsistent data
+consistent_ages = banking[age_equ]
+inconsistent_ages = banking[~age_equ]
+
+# Store consistent and inconsistent data
+print("Number of inconsistent ages: ", inconsistent_ages.shape[0])
 
 # ---------------------------------------------------------
+# Print number of missing values in banking
+print(banking.isna().sum())
 
+# Visualize missingness matrix
+msno.matrix(banking)
+plt.show()
 
 # ---------------------------------------------------------
+# Print number of missing values in banking
+print(banking.isna().sum())
 
+# Visualize missingness matrix
+msno.matrix(banking)
+plt.show()
+
+# Isolate missing and non missing values of inv_amount
+missing_investors = banking[banking["inv_amount"].isna()]
+investors = banking[~banking["inv_amount"].isna()]
 
 # ---------------------------------------------------------
+# Print number of missing values in banking
+print(banking.isna().sum())
 
+# Visualize missingness matrix
+msno.matrix(banking)
+plt.show()
+
+# Isolate missing and non missing values of inv_amount
+missing_investors = banking[banking['inv_amount'].isna()]
+investors = banking[~banking['inv_amount'].isna()]
+
+# Sort banking by age and visualize
+banking_sorted = banking.sort_values(by = "age")
+msno.matrix(banking_sorted)
+plt.show()
 
 # ---------------------------------------------------------
+# Drop missing values of cust_id
+banking_fullid = banking.dropna(subset = ['cust_id'])
 
+# Compute estimated acct_amount
+acct_imp = banking_fullid['inv_amount']*5
+
+# Impute missing acct_amount with corresponding acct_imp
+banking_imputed = banking_fullid.fillna({'acct_amount':acct_imp})
+
+# Print number of missing values
+print(banking_imputed.isna().sum())
 
 # ---------------------------------------------------------
 
