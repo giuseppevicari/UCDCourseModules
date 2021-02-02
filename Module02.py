9d9fb0e8b2aa0e2159d1f5be2185411703d4dceb
gvicari <indirizzoemail@gmail.com>
2021-02-02 12:42:33 +0000

    Completed module 2

--- Module02.py
+++ Module02.py
@@ -846,6 +846,113 @@ banking_imputed = banking_fullid.fillna({'acct_amount':acct_imp})
 print(banking_imputed.isna().sum())
 
 # ---------------------------------------------------------
+# Import process from fuzzywuzzy
+from fuzzywuzzy import process
+
+# Store the unique values of cuisine_type in unique_types
+unique_types = restaurants['cuisine_type'].unique()
+
+# Calculate similarity of 'asian' to all values of unique_types
+print(process.extract('asian', unique_types, limit = len(unique_types)))
+
+# Calculate similarity of 'american' to all values of unique_types
+print(process.extract('american', unique_types, limit = len(unique_types)))
+
+# Calculate similarity of 'italian' to all values of unique_types
+print(process.extract('italian', unique_types, limit = len(unique_types)))
+
+# ---------------------------------------------------------
+# Inspect the unique values of the cuisine_type column
+print(restaurants['cuisine_type'].unique())
+
+# ---------------------------------------------------------
+# Create a list of matches, comparing 'italian' with the cuisine_type column
+matches = process.extract('italian', restaurants['cuisine_type'], limit = len(restaurants))
+
+# Inspect the first 5 matches
+print(matches[0:5])
+
+# ---------------------------------------------------------
+# Create a list of matches, comparing 'italian' with the cuisine_type column
+matches = process.extract('italian', restaurants['cuisine_type'], limit=len(restaurants))
+
+# Iterate through the list of matches to italian
+for match in matches:
+  # Check whether the similarity score is greater than or equal to 80
+  if match[1]>=80:
+    # Select all rows where the cuisine_type is spelled this way, and set them to the correct cuisine
+    restaurants.loc[restaurants['cuisine_type'] == match[0], 'cuisine_type'] = 'italian'
+
+# ---------------------------------------------------------
+# Iterate through categories
+for cuisine in categories:
+    # Create a list of matches, comparing cuisine with the cuisine_type column
+    matches = process.extract(cuisine, restaurants['cuisine_type'], limit=len(restaurants.cuisine_type))
+
+    # Iterate through the list of matches
+    for match in matches:
+        # Check whether the similarity score is greater than or equal to 80
+        if match[1] >= 80:
+            # If it is, select all rows where the cuisine_type is spelled this way, and set them to the correct cuisine
+            restaurants.loc[restaurants['cuisine_type'] == match[0]] = cuisine
+
+# Inspect the final result
+restaurants['cuisine_type'].unique()
+
+# ---------------------------------------------------------
+# Create an indexer and object and find possible pairs
+indexer = recordlinkage.Index()
+
+# Block pairing on cuisine_type
+indexer.block('cuisine_type')
+
+# Generate pairs
+pairs = indexer.index(restaurants, restaurants_new)
+
+# ---------------------------------------------------------
+# Create a comparison object
+comp_cl = recordlinkage.Compare()
+
+# Find exact matches on city, cuisine_types
+comp_cl.exact('city', 'city', label='city')
+comp_cl.exact('cuisine_type', 'cuisine_type', label = 'cuisine_type')
+
+# Find similar matches of rest_name
+comp_cl.string('rest_name', 'rest_name', label='name', threshold = 0.8)
+
+# ---------------------------------------------------------
+# Create a comparison object
+comp_cl = recordlinkage.Compare()
+
+# Find exact matches on city, cuisine_types -
+comp_cl.exact('city', 'city', label='city')
+comp_cl.exact('cuisine_type', 'cuisine_type', label='cuisine_type')
+
+# Find similar matches of rest_name
+comp_cl.string('rest_name', 'rest_name', label='name', threshold = 0.8)
+
+# Get potential matches and print
+potential_matches = comp_cl.compute(pairs, restaurants, restaurants_new)
+print(potential_matches)
+
+# ---------------------------------------------------------
+# Isolate potential matches with row sum >=3
+matches = potential_matches[potential_matches.sum(axis = 1) >= 3]
+
+# Get values of second column index of matches
+matching_indices = matches.index.get_level_values(1)
+
+# Subset restaurants_new based on non-duplicate values
+non_dup = restaurants_new[~restaurants_new.index.isin(matching_indices)]
+
+# Append non_dup to restaurants
+full_restaurants = restaurants.append(non_dup)
+print(full_restaurants)
+
+# ---------------------------------------------------------
+
+
+# ---------------------------------------------------------
 
 
 # ---------------------------------------------------------
