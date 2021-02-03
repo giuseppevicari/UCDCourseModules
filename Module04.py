6fd731fa9cb07623c8ba9437aea0a713ccac4e79
gvicari <indirizzoemail@gmail.com>
2021-02-03 17:30:17 +0000

    Started Module 4

--- Module04.py
+++ Module04.py
@@ -1,26 +1,72 @@
 # ---------------------------------------------------------
-
-
-# ---------------------------------------------------------
-
+# Find characters in movie variable
+length_string = len(movie)
 
 # ---------------------------------------------------------
+# Find characters in movie variable
+length_string = len(movie)
 
-
-# ---------------------------------------------------------
-
-
-# ---------------------------------------------------------
-
+# Convert to string
+to_string = str(len(movie))
 
 # ---------------------------------------------------------
+# Find characters in movie variable
+length_string = len(movie)
 
+# Convert to string
+to_string = str(length_string)
+
+# Predefined variable
+statement = "Number of characters in this review:"
+
+# Concatenate strings and print result
+print(statement + " " + to_string)
 
 # ---------------------------------------------------------
-
+# Select the first 32 characters of movie1
+first_part = movie1[:32]
 
 # ---------------------------------------------------------
+# Select the first 32 characters of movie1
+first_part = movie1[:32]
 
+# Select from 43rd character to the end of movie1
+last_part = movie2[42:]
+
+# ---------------------------------------------------------
+# Select the first 32 characters of movie1
+first_part = movie1[:32]
+
+# Select from 43rd character to the end of movie1
+last_part = movie1[42:]
+
+# Select from 33rd to the 42nd character of movie2
+middle_part = movie2[32:42]
+
+# ---------------------------------------------------------
+# Select the first 32 characters of movie1
+first_part = movie1[:32]
+
+# Select from 43rd character to the end of movie1
+last_part = movie1[42:]
+
+# Select from 33rd to the 42nd character
+middle_part = movie2[32:42]
+
+# Print concatenation and movie2 variable
+print(first_part+middle_part+last_part)
+print(movie2)
+
+# ---------------------------------------------------------
+# Get the word
+movie_title = movie[11:30]
+
+# Obtain the palindrome
+palindrome = movie_title[::-1]
+
+# Print the word if it's a palindrome
+if movie_title == palindrome:
+	print(movie_title)
 
 # ---------------------------------------------------------
 
