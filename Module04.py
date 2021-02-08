f6658226e645585b17623e4a0d5cf63c0f13c536
gvicari <indirizzoemail@gmail.com>
2021-02-08 17:22:16 +0000

    Updated Module 4

--- Module04.py
+++ Module04.py
@@ -271,6 +271,97 @@ print(f"About {fact2:e} of {field2} in the world")
 print(f"{field3} create around {fact3:.2f}% of the data but only {fact4:.1f}% is analyzed")
 
 # ---------------------------------------------------------
+# Include both variables and the result of dividing them
+print(f"{number1} tweets were downloaded in {number2} minutes indicating a speed of {number1/number2:.1f} tweets per min")
+
+# ---------------------------------------------------------
+# Replace the substring https by an empty string
+print(f"{string1.replace('https','' )}")
+
+# ---------------------------------------------------------
+# Divide the length of list by 120 rounded to two decimals
+print(f"Only {len(list_links)*100/120:.2f}% of the posts contain links")
+
+# ---------------------------------------------------------
+# Access values of date and price in east dictionary
+print(f"The price for a house in the east neighborhood was ${east['price']} in {east['date']:%m-%d-%Y}")
+
+# ---------------------------------------------------------
+# Access values of date and price in west dictionary
+print(f"The price for a house in the west neighborhood was ${west['price']} in {west['date']:%m-%d-%Y}.")
+
+# ---------------------------------------------------------
+# Import Template
+from string import Template
+
+# ---------------------------------------------------------
+# Import Template
+from string import Template
+
+# Create a template
+wikipedia = Template("$tool is a $description")
+
+# ---------------------------------------------------------
+# Import Template
+from string import Template
+
+# Create a template
+wikipedia = Template("$tool is a $description")
+
+# Substitute variables in template
+print(wikipedia.substitute(tool=tool1, description=description1))
+print(wikipedia.substitute(tool=tool2, description=description2))
+print(wikipedia.substitute(tool=tool3, description=description3))
+
+# ---------------------------------------------------------
+# Import template
+from string import Template
+
+# Select variables
+our_tool = tools[0]
+our_fee = tools[1]
+our_pay = tools[2]
+
+# Create template
+course = Template("We are offering a 3-month beginner course on $tool just for $$ $fee ${pay}ly")
+
+# Substitute identifiers with three variables
+print(course.substitute(tool=our_tool, fee=our_fee, pay=our_pay))
+
+# ---------------------------------------------------------
+# Import template
+from string import Template
+
+# Complete template string using identifiers
+the_answers = Template("Check your answer 1: $answer1, and your answer 2: $answer2")
+
+# ---------------------------------------------------------
+# Import template
+from string import Template
+
+# Complete template string using identifiers
+the_answers = Template("Check your answer 1: $answer1, and your answer 2: $answer2")
+
+# Use substitute to replace identifiers
+try:
+    print(the_answers.substitute(answers))
+except KeyError:
+    print("Missing information")
+
+# ---------------------------------------------------------
+# Import template
+from string import Template
+
+# Complete template string using identifiers
+the_answers = Template("Check your answer 1: $answer1, and your answer 2: $answer2")
+
+# Use safe_substitute to replace identifiers
+try:
+    print(the_answers.safe_substitute(answers))
+except KeyError:
+    print("Missing information")
+
+# ---------------------------------------------------------
 
 
 # ---------------------------------------------------------
