2a8f5f1eb54b22aaee3c600d9eb8fc83cf97b5e8
gvicari <indirizzoemail@gmail.com>
2021-02-11 18:10:30 +0000

    Completed Module 4

--- Module04.py
+++ Module04.py
@@ -585,33 +585,116 @@ print("Departure: {} Destination: {}".format(flight_matches[0][2], flight_matche
 print("Date: {}".format(flight_matches[0][4]))
 
 # ---------------------------------------------------------
+# Write a regex that matches sentences with the optional words
+regex_positive = r"(love|like|enjoy).+?(movie|concert)\s(.+?)\."
 
+for tweet in sentiment_analysis:
+    # Find all matches of regex in tweet
+    positive_matches = re.findall(regex_positive, tweet)
+
+    # Complete format to print out the results
+    print("Positive comments found {}".format(positive_matches))
+
+# ---------------------------------------------------------
+# Write a regex that matches sentences with the optional words
+regex_negative = r"(hate|dislike|disapprove).+?(?:movie|concert)\s(.+?)\."
+
+for tweet in sentiment_analysis:
+    # Find all matches of regex in tweet
+    negative_matches = re.findall(regex_negative, tweet)
+
+    # Complete format to print out the results
+    print("Negative comments found {}".format(negative_matches))
+
+# ---------------------------------------------------------
+# Write regex and scan contract to capture the dates described
+regex_dates = r"Signed\son\s(\d{2})/(\d{2})/(\d{4})"
+dates = re.search(regex_dates, contract)
+
+# ---------------------------------------------------------
+# Write regex and scan contract to capture the dates described
+regex_dates = r"Signed\son\s(\d{2})/(\d{2})/(\d{4})"
+dates = re.search(regex_dates, contract)
+
+# Assign to each key the corresponding match
+signature = {
+	"day": dates.group(2),
+	"month": dates.group(1),
+	"year": dates.group(3)
+}
+
+# ---------------------------------------------------------
+# Write regex and scan contract to capture the dates described
+regex_dates = r"Signed\son\s(\d{2})/(\d{2})/(\d{4})"
+dates = re.search(regex_dates, contract)
+
+# Assign to each key the corresponding match
+signature = {
+	"day": dates.group(2),
+	"month": dates.group(1),
+	"year": dates.group(3)
+}
+# Complete the format method to print-out
+print("Our first contract is dated back to {data[year]}. Particularly, the day {data[day]} of the month {data[month]}.".format(data=signature))
+
+# ---------------------------------------------------------
+for string in html_tags:
+    # Complete the regex and find if it matches a closed HTML tags
+    match_tag = re.match(r"<(\w+)>.*?</\1>", string)
+
+    if match_tag:
+        # If it matches print the first group capture
+        print("Your tag {} is closed".format(match_tag.group(1)))
+    else:
+        # If it doesn't match capture only the tag
+        notmatch_tag = re.match(r"<(.+?)>", string)
+        # Print the first group capture
+        print("Close your {} tag!".format(notmatch_tag.group(1)))
+
+# ---------------------------------------------------------
+# Complete the regex to match an elongated word
+regex_elongated = r"\w*(\w)\1\w*"
+
+for tweet in sentiment_analysis:
+    # Find if there is a match in each tweet
+    match_elongated = re.search(regex_elongated, tweet)
+
+    if match_elongated:
+        # Assign the captured group zero
+        elongated_word = match_elongated.group(0)
+
+        # Complete the format method to print the word
+        print("Elongated word found: {word}".format(word=elongated_word))
+    else:
+        print("No elongated word found")
+
+# ---------------------------------------------------------
+# Positive lookahead
+look_ahead = re.findall(r"\w+(?=\spython)", sentiment_analysis)
+
+# Print out
+print(look_ahead)
+
+# ---------------------------------------------------------
+# Positive lookbehind
+look_behind = re.findall(r"(?<=[pP]ython\s)\w+", sentiment_analysis)
+
+# Print out
+print(look_behind)
+
+# ---------------------------------------------------------
+for phone in cellphones:
+	# Get all phone numbers not preceded by area code
+	number = re.findall(r"(?<!\d{3}-)\d{4}-\d{6}-\d{2}", phone)
+	print(number)
+# ---------------------------------------------------------
+for phone in cellphones:
+	# Get all phone numbers not followed by optional extension
+	number = re.findall(r"\d{3}-\d{4}-\d{6}(?!-\d{2})", phone)
+	print(number)
 
 # ---------------------------------------------------------
 
-
-# ---------------------------------------------------------
-
-
-# ---------------------------------------------------------
-
-
-# ---------------------------------------------------------
-
-
-# ---------------------------------------------------------
-
-
-# ---------------------------------------------------------
-
-
-# ---------------------------------------------------------
-
-
-# ---------------------------------------------------------
-
-
-# ---------------------------------------------------------
 
 
 
