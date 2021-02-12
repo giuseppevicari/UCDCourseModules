e5d56462cba85a1710ead1a085121ece3f821fe4
gvicari <indirizzoemail@gmail.com>
2021-02-12 14:33:18 +0000

    Started Module 5

--- test.py
+++ test.py
@@ -1,6 +1,20 @@
 import pandas as pd
-data=pd.read_csv("country_vaccinations.csv")
+data=pd.read_csv("netflix_titles.csv")
 #Copy path from content root
 
 print(data.head())
 print(data.shape)
+
+missing_values_count = data.isnull().sum()
+print(missing_values_count[0:1])
+
+"""
+#new_data = data.dropna()
+#new_data = data.dropna(axis = 1) #drops columns
+#print(new_data.shape)
+
+#cleaned_data = data.fillna(0)
+cleaned_data = data.fillna(method='bfill', axis=0).fillna(0)
+missing_values_count = data.isnull().sum()
+print(missing_values_count)
+"""
