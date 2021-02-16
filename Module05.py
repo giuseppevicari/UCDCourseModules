42bb50ed733d7afee057f9e61a902dc26da36990
gvicari <indirizzoemail@gmail.com>
2021-02-16 20:31:21 +0000

    Updated Module 5

--- Module05.py
+++ Module05.py
@@ -117,16 +117,53 @@ andrew = date(1992, 8, 26)
 print(andrew.strftime('%Y-%j'))
 
 # ---------------------------------------------------------
+# Import datetime
+from datetime import datetime
 
+# Create a datetime object
+dt = datetime(2017, 10, 1, 15, 26, 26)
+
+# Print the results in ISO 8601 format
+print(dt.isoformat())
 
 # ---------------------------------------------------------
+# Import datetime
+from datetime import datetime
 
+# Create a datetime object
+dt = datetime(2017, 12, 31, 15, 19, 13)
+
+# Print the results in ISO 8601 format
+print(dt.isoformat())
 
 # ---------------------------------------------------------
+# Import datetime
+from datetime import datetime
 
+# Create a datetime object
+dt = datetime(2017, 12, 31, 15, 19, 13)
+
+# Replace the year with 1917
+dt_old = dt.replace(year = 1917)
+
+# Print the results in ISO 8601 format
+print(dt_old)
 
 # ---------------------------------------------------------
+# Create dictionary to hold results
+trip_counts = {'AM': 0, 'PM': 0}
 
+# Loop over all trips
+for trip in onebike_datetimes:
+    # Check to see if the trip starts before noon
+    if trip['start'].hour < 12:
+        # Increment the counter for before noon
+        trip_counts['AM'] += 1
+    else:
+        # Increment the counter for after noon
+        trip_counts['PM'] += 1
+
+print(trip_counts)
 
 # ---------------------------------------------------------
 
