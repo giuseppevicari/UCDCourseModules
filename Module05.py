941c0f7b2e8ec36a8f6fe9a9a832d15e037087f5
gvicari <indirizzoemail@gmail.com>
2021-02-19 16:52:44 +0000

    Completed Module 5

--- Module05.py
+++ Module05.py
@@ -484,37 +484,119 @@ for trip in onebike_datetimes:
 print("Shortest trip: " + str(min(trip_durations)))
 
 # ---------------------------------------------------------
+# Import pandas
+import pandas as pd
 
+# Load CSV into the rides variable
+rides = pd.read_csv('capital-onebike.csv',
+                    parse_dates = ['Start date', 'End date'])
 
-# ---------------------------------------------------------
-
-
-# ---------------------------------------------------------
-
-
-# ---------------------------------------------------------
-
-
-# ---------------------------------------------------------
-
+# Print the initial (0th) row
+print(rides.iloc[0])
 
 # ---------------------------------------------------------
+# Subtract the start date from the end date
+ride_durations = rides['End date'] - rides['Start date']
 
+# Convert the results to seconds
+rides['Duration'] = ride_durations.dt.total_seconds()
 
-# ---------------------------------------------------------
-
-
-# ---------------------------------------------------------
-
-
-# ---------------------------------------------------------
-
+print(rides['Duration'].head())
 
 # ---------------------------------------------------------
+# Create joyrides
+joyrides = (rides['Start station'] == rides['End station'])
 
+# Total number of joyrides
+print("{} rides were joyrides".format(joyrides.sum()))
+
+# Median of all rides
+print("The median duration overall was {:.2f} seconds"\
+      .format(rides['Duration'].median()))
+
+# Median of joyrides
+print("The median duration for joyrides was {:.2f} seconds"\
+      .format(rides[joyrides]['Duration'].median()))
 
 # ---------------------------------------------------------
+# Import matplotlib
+import matplotlib.pyplot as plt
 
+# Resample rides to daily, take the size, plot the results
+rides.resample('D', on = 'Start date')\
+  .size()\
+  .plot(ylim = [0, 15])
+
+# Show the results
+plt.show()
+
+# ---------------------------------------------------------
+# Import matplotlib
+import matplotlib.pyplot as plt
+
+# Resample rides to monthly, take the size, plot the results
+rides.resample('M', on = 'Start date')\
+  .size()\
+  .plot(ylim = [0, 150])
+
+# Show the results
+plt.show()
+
+# ---------------------------------------------------------
+# Resample rides to be monthly on the basis of Start date
+monthly_rides = rides.resample('M', on = 'Start date')['Member type']
+
+# Take the ratio of the .value_counts() over the total number of rides
+print(monthly_rides.value_counts() / monthly_rides.size())
+
+# ---------------------------------------------------------
+# Group rides by member type, and resample to the month
+grouped = rides.groupby('Member type')\
+  .resample('M', on = 'Start date')
+
+# Print the median duration for each group
+print(grouped['Duration'].median())
+
+# ---------------------------------------------------------
+# Localize the Start date column to America/New_York
+rides['Start date'] = rides['Start date'].dt.tz_localize('America/New_York', ambiguous = 'NaT')
+
+# Print first value
+print(rides['Start date'].iloc[0])
+
+# ---------------------------------------------------------
+# Localize the Start date column to America/New_York
+rides['Start date'] = rides['Start date'].dt.tz_localize('America/New_York',
+                                						 ambiguous='NaT')
+
+# Print first value
+print(rides['Start date'].iloc[0])
+
+# Convert the Start date column to Europe/London
+rides['Start date'] = rides['Start date'].dt.tz_convert('Europe/London')
+
+# Print the new value
+print(rides['Start date'].iloc[0])
+
+# ---------------------------------------------------------
+# Add a column for the weekday of the start of the ride
+rides['Ride start weekday'] = rides['Start date'].dt.weekday_name
+
+# Print the median trip time per weekday
+print(rides.groupby('Ride start weekday')['Duration'].median())
+
+# ---------------------------------------------------------
+# Shift the index of the end date up one; now subract it from the start date
+rides['Time since'] = rides['Start date'] - (rides['End date'].shift(1))
+
+# Move from a timedelta to a number of seconds, which is easier to work with
+rides['Time since'] = rides['Time since'].dt.total_seconds()
+
+# Resample to the month
+monthly = rides.resample('M', on = 'Start date')
+
+# Print the average hours between rides each month
+print(monthly['Time since'].mean()/(60*60))
 
 # ---------------------------------------------------------
 
