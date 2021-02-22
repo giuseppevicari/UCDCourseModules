2696264e3f668e46bc94d2a44b54e6c76fbbd6f9
gvicari <indirizzoemail@gmail.com>
2021-02-22 20:58:58 +0000

    Updated Module 6

--- Module06.py
+++ Module06.py
@@ -143,7 +143,328 @@ def better_add_column(values, df=None):
   return df
 
 # ---------------------------------------------------------
+# Open "alice.txt" and assign the file to "file"
+with open('alice.txt') as file:
+  text = file.read()
 
+n = 0
+for word in text.split():
+  if word.lower() in ['cat', 'cats']:
+    n += 1
+
+print('Lewis Carroll uses the word "cat" {} times'.format(n))
+
+# ---------------------------------------------------------
+image = get_image_from_instagram()
+
+# Time how long process_with_numpy(image) takes to run
+with timer():
+  print('Numpy version')
+  process_with_numpy(image)
+
+# Time how long process_with_pytorch(image) takes to run
+with timer():
+  print('Pytorch version')
+  process_with_pytorch(image)
+
+# ---------------------------------------------------------
+# Add a decorator that will make timer() a context manager
+@contextlib.contextmanager
+def timer():
+  """Time the execution of a context block.
+
+  Yields:
+    None
+  """
+  start = time.time()
+  # Send control back to the context block
+  yield
+  end = time.time()
+  print('Elapsed: {:.2f}s'.format(end - start))
+
+with timer():
+  print('This should take approximately 0.25 seconds')
+  time.sleep(0.25)
+
+# ---------------------------------------------------------
+@contextlib.contextmanager
+def open_read_only(filename):
+  """Open a file in read-only mode.
+
+  Args:
+    filename (str): The location of the file to read
+
+  Yields:
+    file object
+  """
+  read_only_file = open(filename, mode='r')
+  # Yield read_only_file so it can be assigned to my_file
+  yield read_only_file
+  # Close read_only_file
+  read_only_file.close()
+
+with open_read_only('my_file.txt') as my_file:
+  print(my_file.read())
+
+# ---------------------------------------------------------
+# Use the "stock('NVDA')" context manager
+# and assign the result to the variable "nvda"
+with stock('NVDA') as nvda:
+  # Open "NVDA.txt" for writing as f_out
+  with open('NVDA.txt', 'w') as f_out:
+    for _ in range(10):
+      value = nvda.price()
+      print('Logging ${:.2f} for NVDA'.format(value))
+      f_out.write('{:.2f}\n'.format(value))
+
+# ---------------------------------------------------------
+def in_dir(directory):
+  """Change current working directory to `directory`,
+  allow the user to run some code, and change back.
+
+  Args:
+    directory (str): The path to a directory to work in.
+  """
+  current_dir = os.getcwd()
+  os.chdir(directory)
+
+  # Add code that lets you handle errors
+  try:
+    yield
+  # Ensure the directory is reset,
+  # whether there was an error or not
+  finally:
+    os.chdir(current_dir)
+
+# ---------------------------------------------------------
+# Add the missing function references to the function map
+function_map = {
+  'mean': mean,
+  'std': std,
+  'minimum': minimum,
+  'maximum': maximum
+}
+
+data = load_data()
+print(data)
+
+func_name = get_user_input()
+
+# Call the chosen function and pass "data" as an argument
+function_map[func_name](data)
+
+# ---------------------------------------------------------
+# Call has_docstring() on the load_and_plot_data() function
+ok = has_docstring(load_and_plot_data)
+
+if not ok:
+  print("load_and_plot_data() doesn't have a docstring!")
+else:
+  print("load_and_plot_data() looks ok")
+
+# ---------------------------------------------------------
+# Call has_docstring() on the as_2D() function
+ok = has_docstring(as_2D)
+
+if not ok:
+  print("as_2D() doesn't have a docstring!")
+else:
+  print("as_2D() looks ok")
+
+# ---------------------------------------------------------
+# Call has_docstring() on the log_product() function
+ok = has_docstring(log_product)
+
+if not ok:
+  print("log_product() doesn't have a docstring!")
+else:
+  print("log_product() looks ok")
+
+# ---------------------------------------------------------
+def create_math_function(func_name):
+  if func_name == 'add':
+    def add(a, b):
+      return a + b
+
+    return add
+  elif func_name == 'subtract':
+    # Define the subtract() function
+    def subtract(a, b):
+      return a - b
+
+    return subtract
+  else:
+    print("I don't know that one")
+
+
+add = create_math_function('add')
+print('5 + 2 = {}'.format(add(5, 2)))
+
+subtract = create_math_function('subtract')
+print('5 - 2 = {}'.format(subtract(5, 2)))
+
+# ---------------------------------------------------------
+call_count = 0
+
+
+def my_function():
+  # Use a keyword that lets us update call_count
+  global call_count
+  call_count += 1
+
+  print("You've called my_function() {} times!".format(
+    call_count
+  ))
+
+
+for _ in range(20):
+  my_function()
+
+# ---------------------------------------------------------
+def read_files():
+  file_contents = None
+
+  def save_contents(filename):
+    # Add a keyword that lets us modify file_contents
+    nonlocal file_contents
+    if file_contents is None:
+      file_contents = []
+    with open(filename) as fin:
+      file_contents.append(fin.read())
+
+  for filename in ['1984.txt', 'MobyDick.txt', 'CatsEye.txt']:
+    save_contents(filename)
+
+  return file_contents
+
+
+print('\n'.join(read_files()))
+
+# ---------------------------------------------------------
+def wait_until_done():
+  def check_is_done():
+    # Add a keyword so that wait_until_done()
+    # doesn't run forever
+    global done
+    if random.random() < 0.1:
+      done = True
+
+  while not done:
+    check_is_done()
+
+
+done = False
+wait_until_done()
+
+print('Work done? {}'.format(done))
+
+# ---------------------------------------------------------
+def return_a_func(arg1, arg2):
+  def new_func():
+    print('arg1 was {}'.format(arg1))
+    print('arg2 was {}'.format(arg2))
+
+  return new_func
+
+
+my_func = return_a_func(2, 17)
+
+# Show that my_func()'s closure is not None
+print(my_func.__closure__ is not None)
+
+# ---------------------------------------------------------
+def return_a_func(arg1, arg2):
+  def new_func():
+    print('arg1 was {}'.format(arg1))
+    print('arg2 was {}'.format(arg2))
+
+  return new_func
+
+
+my_func = return_a_func(2, 17)
+
+print(my_func.__closure__ is not None)
+
+# Show that there are two variables in the closure
+print(len(my_func.__closure__) == 2)
+
+# ---------------------------------------------------------
+def return_a_func(arg1, arg2):
+  def new_func():
+    print('arg1 was {}'.format(arg1))
+    print('arg2 was {}'.format(arg2))
+
+  return new_func
+
+
+my_func = return_a_func(2, 17)
+
+print(my_func.__closure__ is not None)
+print(len(my_func.__closure__) == 2)
+
+# Get the values of the variables in the closure
+closure_values = [
+  my_func.__closure__[i].cell_contents for i in range(2)
+]
+print(closure_values == [2, 17])
+
+# ---------------------------------------------------------
+def my_special_function():
+  print('You are running my_special_function()')
+
+
+def get_new_func(func):
+  def call_func():
+    func()
+
+  return call_func
+
+
+new_func = get_new_func(my_special_function)
+
+
+# Redefine my_special_function() to just print "hello"
+def my_special_function():
+  print('hello')
+
+
+new_func()
+
+# ---------------------------------------------------------
+def my_special_function():
+  print('You are running my_special_function()')
+
+
+def get_new_func(func):
+  def call_func():
+    func()
+
+  return call_func
+
+
+new_func = get_new_func(my_special_function)
+
+# Delete my_special_function()
+del (my_special_function)
+
+new_func()
+
+# ---------------------------------------------------------
+def my_special_function():
+  print('You are running my_special_function()')
+
+
+def get_new_func(func):
+  def call_func():
+    func()
+
+  return call_func
+
+
+# Overwrite `my_special_function` with the new function
+my_special_function = get_new_func(my_special_function)
+
+my_special_function()
 
 # ---------------------------------------------------------
 
@@ -157,16 +478,7 @@ def better_add_column(values, df=None):
 # ---------------------------------------------------------
 
 
-# ---------------------------------------------------------
 
-
-# ---------------------------------------------------------
-
-
-# ---------------------------------------------------------
-
-
-# ---------------------------------------------------------
 
 
 
--- test.py
+++ test.py
@@ -1,20 +1,7 @@
-import pandas as pd
-data=pd.read_csv("netflix_titles.csv")
-#Copy path from content root
+x= 1
 
-print(data.head())
-print(data.shape)
+def funz():
+    x=2
+    print('ics =', x)
 
-missing_values_count = data.isnull().sum()
-print(missing_values_count[0:1])
-
-"""
-#new_data = data.dropna()
-#new_data = data.dropna(axis = 1) #drops columns
-#print(new_data.shape)
-
-#cleaned_data = data.fillna(0)
-cleaned_data = data.fillna(method='bfill', axis=0).fillna(0)
-missing_values_count = data.isnull().sum()
-print(missing_values_count)
-"""
+funz()
