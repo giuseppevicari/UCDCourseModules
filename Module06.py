bd1a68eae0ec53235488c25d84032ce4b9316c0c
gvicari <indirizzoemail@gmail.com>
2021-02-23 17:51:43 +0000

    Updated Module 6

--- Module06.py
+++ Module06.py
@@ -467,6 +467,204 @@ my_special_function = get_new_func(my_special_function)
 my_special_function()
 
 # ---------------------------------------------------------
+def my_function(a, b, c):
+  print(a + b + c)
+
+# Decorate my_function() with the print_args() decorator
+my_function = print_args(my_function)
+
+my_function(1, 2, 3)
+
+# ---------------------------------------------------------
+# Decorate my_function() with the print_args() decorator
+@print_args
+def my_function(a, b, c):
+  print(a + b + c)
+
+my_function(1, 2, 3)
+
+# ---------------------------------------------------------
+def print_before_and_after(func):
+  def wrapper(*args):
+    print('Before {}'.format(func.__name__))
+    # Call the function being decorated with *args
+    func(*args)
+    print('After {}'.format(func.__name__))
+  # Return the nested function
+  return wrapper
+
+@print_before_and_after
+def multiply(a, b):
+  print(a * b)
+
+multiply(5, 10)
+
+# ---------------------------------------------------------
+def print_return_type(func):
+  # Define wrapper(), the decorated function
+  def wrapper(*args, **kwargs):
+    # Call the function being decorated
+    result = func(*args, **kwargs)
+    print('{}() returned type {}'.format(
+      func.__name__, type(result)
+    ))
+    return result
+
+  # Return the decorated function
+  return wrapper
+
+
+@print_return_type
+def foo(value):
+  return value
+
+
+print(foo(42))
+print(foo([1, 2, 3]))
+print(foo({'a': 42}))
+
+# ---------------------------------------------------------
+def counter(func):
+  def wrapper(*args, **kwargs):
+    wrapper.count += 1
+    # Call the function being decorated and return the result
+    return func(*args, **kwargs)
+
+  wrapper.count = 0
+  # Return the new decorated function
+  return wrapper
+
+
+# Decorate foo() with the counter() decorator
+@counter
+def foo():
+  print('calling foo()')
+
+
+foo()
+foo()
+
+print('foo() was called {} times.'.format(foo.count))
+
+# ---------------------------------------------------------
+def add_hello(func):
+  def wrapper(*args, **kwargs):
+    print('Hello')
+    return func(*args, **kwargs)
+
+  return wrapper
+
+
+# Decorate print_sum() with the add_hello() decorator
+@add_hello
+def print_sum(a, b):
+  """Adds two numbers and prints the sum"""
+  print(a + b)
+
+
+print_sum(10, 20)
+print(print_sum.__doc__)
+
+# ---------------------------------------------------------
+def add_hello(func):
+  # Add a docstring to wrapper
+  def wrapper(*args, **kwargs):
+    """Print 'hello' and then call the decorated function."""
+    print('Hello')
+    return func(*args, **kwargs)
+
+  return wrapper
+
+
+@add_hello
+def print_sum(a, b):
+  """Adds two numbers and prints the sum"""
+  print(a + b)
+
+
+print_sum(10, 20)
+print(print_sum.__doc__)
+
+# ---------------------------------------------------------
+# Import the function you need to fix the problem
+from functools import wraps
+
+
+def add_hello(func):
+  def wrapper(*args, **kwargs):
+    """Print 'hello' and then call the decorated function."""
+    print('Hello')
+    return func(*args, **kwargs)
+
+  return wrapper
+
+
+@add_hello
+def print_sum(a, b):
+  """Adds two numbers and prints the sum"""
+  print(a + b)
+
+
+print_sum(10, 20)
+print(print_sum.__doc__)
+
+# ---------------------------------------------------------
+from functools import wraps
+
+
+def add_hello(func):
+  # Decorate wrapper() so that it keeps func()'s metadata
+  @wraps(func)
+  def wrapper(*args, **kwargs):
+    """Print 'hello' and then call the decorated function."""
+    print('Hello')
+    return func(*args, **kwargs)
+
+  return wrapper
+
+
+@add_hello
+def print_sum(a, b):
+  """Adds two numbers and prints the sum"""
+  print(a + b)
+
+
+print_sum(10, 20)
+print(print_sum.__doc__)
+
+# ---------------------------------------------------------
+@check_everything
+def duplicate(my_list):
+  """Return a new list that repeats the input twice"""
+  return my_list + my_list
+
+t_start = time.time()
+duplicated_list = duplicate(list(range(50)))
+t_end = time.time()
+decorated_time = t_end - t_start
+
+t_start = time.time()
+# Call the original function instead of the decorated one
+duplicated_list = duplicate.__wrapped__(list(range(50)))
+t_end = time.time()
+undecorated_time = t_end - t_start
+
+print('Decorated time: {:.5f}s'.format(decorated_time))
+print('Undecorated time: {:.5f}s'.format(undecorated_time))
+
+# ---------------------------------------------------------
+
+
+# ---------------------------------------------------------
+
+
+# ---------------------------------------------------------
+
+
+# ---------------------------------------------------------
+
+
+# ---------------------------------------------------------
 
 
 # ---------------------------------------------------------
