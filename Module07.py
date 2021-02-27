b4642cd2ec86cf40906a903f90bf2dc103115bca
gvicari <indirizzoemail@gmail.com>
2021-02-27 21:47:26 +0000

    Updated module 7

--- Module07.py
+++ Module07.py
@@ -177,19 +177,80 @@ plt.ylabel('CDF')
 plt.show()
 
 # ---------------------------------------------------------
+# Select educ
+educ = gss['educ']
 
+# Bachelor's degree
+bach = (educ >= 16)
+
+# Associate degree
+assc = ((educ >=14) & (educ <16))
+
+# High school (12 or fewer years of education)
+high = (educ <=12)
+print(high.mean())
 
 # ---------------------------------------------------------
+income = gss['realinc']
 
+# Plot the CDFs
+Cdf(income[high]).plot(label='High school')
+Cdf(income[assc]).plot(label='Associate')
+Cdf(income[bach]).plot(label='Bachelor')
+
+# Label the axes
+plt.xlabel('Income (1986 USD)')
+plt.ylabel('CDF')
+plt.legend()
+plt.show()
 
 # ---------------------------------------------------------
+# Extract realinc and compute its log
+income = gss['realinc']
+log_income = np.log10(income)
 
+# Compute mean and standard deviation
+mean = log_income.mean()
+std = log_income.std()
+print(mean, std)
+
+# Make a norm object
+from scipy.stats import norm
+dist = norm(mean, std)
 
 # ---------------------------------------------------------
+# Evaluate the model CDF
+xs = np.linspace(2, 5.5)
+ys = dist.cdf(xs)
 
+# Plot the model CDF
+plt.clf()
+plt.plot(xs, ys, color='gray')
+
+# Create and plot the Cdf of log_income
+Cdf(log_income).plot()
+
+# Label the axes
+plt.xlabel('log10 of realinc')
+plt.ylabel('CDF')
+plt.show()
 
 # ---------------------------------------------------------
+# Evaluate the normal PDF
+xs = np.linspace(2, 5.5)
+ys = dist.pdf(xs)
 
+# Plot the model PDF
+plt.clf()
+plt.plot(xs, ys, color='gray')
+
+# Plot the data KDE
+sns.kdeplot(log_income)
+
+# Label the axes
+plt.xlabel('log10 of realinc')
+plt.ylabel('PDF')
+plt.show()
 
 # ---------------------------------------------------------
 
