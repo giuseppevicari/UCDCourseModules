d83802049939a155ee600132bcacb5021bf85a4a
gvicari <indirizzoemail@gmail.com>
2021-03-04 18:23:56 +0000

    Updated Module 7

--- Module07.py
+++ Module07.py
@@ -416,22 +416,122 @@ results = smf.ols('realinc ~ educ + educ2 + age + age2', data = gss).fit()
 print(results.params)
 
 # ---------------------------------------------------------
+# Run a regression model with educ, educ2, age, and age2
+results = smf.ols('realinc ~ educ + educ2 + age + age2', data=gss).fit()
 
+# Make the DataFrame
+df = pd.DataFrame()
+df['educ'] = np.linspace(0,20)
+df['age'] = 30
+df['educ2'] = df['educ']**2
+df['age2'] = df['age']**2
+
+# Generate and plot the predictions
+pred = results.predict(df)
+print(pred.head())
 
 # ---------------------------------------------------------
+# Plot mean income in each age group
+plt.clf()
+grouped = gss.groupby('educ')
+mean_income_by_educ = grouped['realinc'].mean()
+plt.plot(mean_income_by_educ, 'o', alpha = 0.5)
 
+# Plot the predictions
+pred = results.predict(df)
+plt.plot(df['educ'], pred, label='Age 30')
+
+# Label axes
+plt.xlabel('Education (years)')
+plt.ylabel('Income (1986 $)')
+plt.legend()
+plt.show()
 
 # ---------------------------------------------------------
+# Recode grass
+gss['grass'].replace(2, 0, inplace=True)
 
+# Run logistic regression
+results = smf.logit('grass ~ age + age2 + educ + educ2 + C(sex)', data = gss).fit()
+results.params
 
 # ---------------------------------------------------------
+# Recode grass
+gss['grass'].replace(2, 0, inplace=True)
 
+# Run logistic regression
+results = smf.logit('grass ~ age + age2 + educ + educ2 + C(sex)', data=gss).fit()
+results.params
+
+# Make a DataFrame with a range of ages
+df = pd.DataFrame()
+df['age'] = np.linspace(18, 89)
+df['age2'] = df['age']**2
+
+# Set the education level to 12
+df['educ'] = 12
+df['educ2'] = df['educ']**2
 
 # ---------------------------------------------------------
+# Recode grass
+gss['grass'].replace(2, 0, inplace=True)
 
+# Run logistic regression
+results = smf.logit('grass ~ age + age2 + educ + educ2 + C(sex)', data=gss).fit()
+results.params
+
+# Make a DataFrame with a range of ages
+df = pd.DataFrame()
+df['age'] = np.linspace(18, 89)
+df['age2'] = df['age']**2
+
+# Set the education level to 12
+df['educ'] = 12
+df['educ2'] = df['educ']**2
+
+# Generate predictions for men and women
+df['sex'] = 1
+pred1 = results.predict(df)
+
+df['sex'] = 2
+pred2 = results.predict(df)
 
 # ---------------------------------------------------------
+# Recode grass
+gss['grass'].replace(2, 0, inplace=True)
 
+# Run logistic regression
+results = smf.logit('grass ~ age + age2 + educ + educ2 + C(sex)', data=gss).fit()
+results.params
+
+# Make a DataFrame with a range of ages
+df = pd.DataFrame()
+df['age'] = np.linspace(18, 89)
+df['age2'] = df['age']**2
+
+# Set the education level to 12
+df['educ'] = 12
+df['educ2'] = df['educ']**2
+
+# Generate predictions for men and women
+df['sex'] = 1
+pred1 = results.predict(df)
+
+df['sex'] = 2
+pred2 = results.predict(df)
+
+plt.clf()
+grouped = gss.groupby('age')
+favor_by_age = grouped.mean()
+plt.plot(favor_by_age, 'o', alpha=0.5)
+
+plt.plot(df['age'], pred1, label='Male')
+plt.plot(df['age'], pred2, label='Female')
+
+plt.xlabel('Age')
+plt.ylabel('Probability of favoring legalization')
+plt.legend()
+plt.show()
 
 # ---------------------------------------------------------
 
