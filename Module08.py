bfc43f21de18ee6d0966929c762b4adcd354c8c6
gvicari <indirizzoemail@gmail.com>
2021-03-07 19:43:24 +0000

    Started Module 8

---
+++ Module08.py
@@ -0,0 +1,296 @@
+# ---------------------------------------------------------
+# Import plotting modules
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+# Set default Seaborn style
+sns.set()
+
+# Plot histogram of versicolor petal lengths
+plt.hist(versicolor_petal_length)
+
+# Show histogram
+plt.show()
+
+# ---------------------------------------------------------
+# Plot histogram of versicolor petal lengths
+_ = plt.hist(versicolor_petal_length)
+
+# Label axes
+plt.xlabel('petal length (cm)')
+plt.ylabel('count')
+
+# Show histogram
+plt.show()
+
+# ---------------------------------------------------------
+# Import numpy
+import numpy as np
+
+# Compute number of data points: n_data
+n_data = len(versicolor_petal_length)
+
+# Number of bins is the square root of number of data points: n_bins
+n_bins = np.sqrt(n_data)
+
+# Convert number of bins to integer: n_bins
+n_bins = int(n_bins)
+
+# Plot the histogram
+plt.hist(versicolor_petal_length, bins = n_bins)
+
+# Label axes
+_ = plt.xlabel('petal length (cm)')
+_ = plt.ylabel('count')
+
+# Show histogram
+plt.show()
+
+# ---------------------------------------------------------
+# Create bee swarm plot with Seaborn's default settings
+_ = sns.swarmplot(x='species', y='petal length (cm)', data = df)
+
+# Label the axes
+_ = plt.xlabel('species')
+_ = plt.ylabel('petal length')
+
+# Show the plot
+plt.show()
+
+# ---------------------------------------------------------
+def ecdf(data):
+    """Compute ECDF for a one-dimensional array of measurements."""
+    # Number of data points: n
+    n = len(data)
+
+    # x-data for the ECDF: x
+    x = np.sort(data)
+
+    # y-data for the ECDF: y
+    y = np.arange(1, n+1) / n
+
+    return x, y
+
+# ---------------------------------------------------------
+# Compute ECDF for versicolor data: x_vers, y_vers
+x_vers, y_vers = ecdf(versicolor_petal_length)
+
+# Generate plot
+_ = plt.plot(x_vers, y_vers, marker = '.', linestyle = 'none')
+
+# Label the axes
+_ = plt.xlabel('petal length')
+_ = plt.ylabel('ECDF')
+
+# Display the plot
+plt.show()
+
+# ---------------------------------------------------------
+# Compute ECDFs
+x_set, y_set = ecdf(setosa_petal_length)
+x_vers, y_vers = ecdf(versicolor_petal_length)
+x_virg, y_virg = ecdf(virginica_petal_length)
+
+# Plot all ECDFs on the same plot
+_ = plt.plot(x_set, y_set, marker = '.', linestyle = 'none')
+_ = plt.plot(x_vers, y_vers, marker = '.', linestyle = 'none')
+_ = plt.plot(x_virg, y_virg, marker = '.', linestyle = 'none')
+
+# Annotate the plot
+plt.legend(('setosa', 'versicolor', 'virginica'), loc='lower right')
+_ = plt.xlabel('petal length (cm)')
+_ = plt.ylabel('ECDF')
+
+# Display the plot
+plt.show()
+
+# ---------------------------------------------------------
+# Compute the mean: mean_length_vers
+mean_length_vers = np.mean(versicolor_petal_length)
+
+# Print the result with some nice formatting
+print('I. versicolor:', mean_length_vers, 'cm')
+
+# ---------------------------------------------------------
+Course Outline
+Daily XP 1350
+
+
+
+
+Exercise
+Exercise
+Computing percentiles
+In this exercise, you will compute the percentiles of petal length of Iris versicolor.
+
+Instructions
+100 XP
+Create percentiles, a NumPy array of percentiles you want to compute. These are the 2.5th, 25th, 50th, 75th, and 97.5th. You can do so by creating a list containing these ints/floats and convert the list to a NumPy array using np.array(). For example, np.array([30, 50]) would create an array consisting of the 30th and 50th percentiles.
+Use np.percentile() to compute the percentiles of the petal lengths from the Iris versicolor samples. The variable versicolor_petal_length is in your namespace.
+Print the percentiles.
+
+Take Hint (-30 XP)
+script.py
+
+Light Mode
+12345678
+# Specify array of percentiles: percentiles
+percentiles = np.array([2.5, 25, 50, 75, 97.5])
+
+# Compute percentiles: ptiles_vers
+ptiles_vers = np.percentile(versicolor_petal_length, percentiles)
+
+# Print the result
+print (ptiles_vers)
+
+
+Run Code
+
+Submit Answer
+IPython Shell
+Slides
+
+# Compute the mean: mean_length_vers
+mean_length_vers = np.mean(versicolor_petal_length)
+
+# Print the result with some nice formatting
+print('I. versicolor:', mean_length_vers, 'cm')
+# Specify array of percentiles: percentiles
+percentiles = np.array([2.5, 25, 50, 75, 97.5])
+
+# Compute percentiles: ptiles_vers
+ptiles_vers = np.percentile(versicolor_petal_length, percentiles)
+
+# Print the result
+print ptiles_vers
+  File "<stdin>", line 8
+    print ptiles_vers
+                    ^
+SyntaxError: Missing parentheses in call to 'print'. Did you mean print(ptiles_vers)?
+# Specify array of percentiles: percentiles
+percentiles = np.array([2.5, 25, 50, 75, 97.5])
+
+# Compute percentiles: ptiles_vers
+ptiles_vers = np.percentile(versicolor_petal_length, percentiles)
+
+# Print the result
+print (ptiles_vers)
+[3.3    4.     4.35   4.6    4.9775]
+In [1]:
+;
+ptiles_vers
+
+# ---------------------------------------------------------
+# Plot the ECDF
+_ = plt.plot(x_vers, y_vers, '.')
+_ = plt.xlabel('petal length (cm)')
+_ = plt.ylabel('ECDF')
+
+# Overlay percentiles as red diamonds.
+_ = plt.plot(ptiles_vers, percentiles/100, marker='D', color='red',
+         linestyle='none')
+
+# Show the plot
+plt.show()
+
+# ---------------------------------------------------------
+# Create box plot with Seaborn's default settings
+_ = sns.boxplot(x='species', y='petal length (cm)', data = df)
+
+# Label the axes
+_ = plt.xlabel('species')
+_ = plt.ylabel('petal length')
+
+# Show the plot
+plt.show()
+
+# ---------------------------------------------------------
+# Array of differences to mean: differences
+differences = versicolor_petal_length - np.mean(versicolor_petal_length)
+
+# Square the differences: diff_sq
+diff_sq = differences**2
+
+# Compute the mean square difference: variance_explicit
+variance_explicit = np.mean(diff_sq)
+
+# Compute the variance using NumPy: variance_np
+variance_np = np.var(versicolor_petal_length)
+
+# Print the results
+print(variance_explicit, variance_np)
+
+# ---------------------------------------------------------
+# Compute the variance: variance
+variance = np.var(versicolor_petal_length)
+
+# Print the square root of the variance
+print(np.sqrt(variance))
+
+# Print the standard deviation
+print(np.std(versicolor_petal_length))
+
+# ---------------------------------------------------------
+# Make a scatter plot
+_ = plt.plot(versicolor_petal_length, versicolor_petal_width, marker = '.', linestyle = 'none')
+
+# Label the axes
+_ = plt.xlabel('length')
+_ = plt.ylabel('width')
+
+# Show the result
+plt.show()
+
+# ---------------------------------------------------------
+# Compute the covariance matrix: covariance_matrix
+covariance_matrix = np.cov(versicolor_petal_length, versicolor_petal_width)
+
+# Print covariance matrix
+print(covariance_matrix)
+
+# Extract covariance of length and width of petals: petal_cov
+petal_cov = covariance_matrix[1,0]
+
+# Print the length/width covariance
+print(petal_cov)
+
+# ---------------------------------------------------------
+def pearson_r(x, y):
+    """Compute Pearson correlation coefficient between two arrays."""
+    # Compute correlation matrix: corr_mat
+    corr_mat = np.corrcoef(x, y)
+
+    # Return entry [0,1]
+    return corr_mat[0,1]
+
+# Compute Pearson correlation coefficient for I. versicolor: r
+r = pearson_r(versicolor_petal_length, versicolor_petal_width)
+
+# Print the result
+print(r)
+
+# ---------------------------------------------------------
+
+
+# ---------------------------------------------------------
+
+
+# ---------------------------------------------------------
+
+
+# ---------------------------------------------------------
+
+
+# ---------------------------------------------------------
+
+
+# ---------------------------------------------------------
+
+
+# ---------------------------------------------------------
+
+
+# ---------------------------------------------------------
+
+
--- test.py
+++ test.py
@@ -1,4 +1,14 @@
-import requests
-request = requests.get('http://api.open-notify.org/iss-now.json')
-data = request.json()
-print(data['iss_position'])
+import pandas as pd
+canadian_youtube = pd.read_csv("CAvideos.csv")
+british_youtube = pd.read_csv("GBvideos.csv")
+#print(british_youtube.head())
+#print(british_youtube.info())
+print(canadian_youtube.shape)
+print(british_youtube.shape)
+concat_data = pd.concat([canadian_youtube, british_youtube])
+print(concat_data.shape)
+
+left = canadian_youtube.set_index(['title', 'trending_date'])
+right = british_youtube.set_index(['title', 'trending_date'])
+join_data = left.join(right, lsuffix='_CAN', rsuffix='_UK')
+merged_data = pd.merge(left, right, on = 'video_id')
