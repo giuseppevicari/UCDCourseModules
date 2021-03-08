1e9cc07ec80751f94187d5e3e52c67f5055c5a46
gvicari <indirizzoemail@gmail.com>
2021-03-08 17:11:17 +0000

    Started Module 8

--- Module08.py
+++ Module08.py
@@ -271,21 +271,226 @@ r = pearson_r(versicolor_petal_length, versicolor_petal_width)
 print(r)
 
 # ---------------------------------------------------------
+# Seed the random number generator
+np.random.seed(42)
 
+# Initialize random numbers: random_numbers
+random_numbers = np.empty(100000)
+
+# Generate random numbers by looping over range(100000)
+for i in range(100000):
+    random_numbers[i] = np.random.random()
+
+# Plot a histogram
+_ = plt.hist(random_numbers)
+
+# Show the plot
+plt.show()
 
 # ---------------------------------------------------------
+def perform_bernoulli_trials(n, p):
+    """Perform n Bernoulli trials with success probability p
+    and return number of successes."""
+    # Initialize number of successes: n_success
+    n_success = 0
 
+    # Perform trials
+    for i in range(n):
+        # Choose random number between zero and one: random_number
+        random_number = np.random.random()
+
+        # If less than p, it's a success so add one to n_success
+        if random_number < p:
+            n_success +=1
+
+    return n_success
 
 # ---------------------------------------------------------
+# Seed random number generator
+np.random.seed(42)
 
+# Initialize the number of defaults: n_defaults
+n_defaults = np.empty(1000)
+
+# Compute the number of defaults
+for i in range(1000):
+    n_defaults[i] = perform_bernoulli_trials(100, 0.05)
+
+
+# Plot the histogram with default number of bins; label your axes
+_ = plt.hist(n_defaults, normed = True)
+_ = plt.xlabel('number of defaults out of 100 loans')
+_ = plt.ylabel('probability')
+
+# Show the plot
+plt.show()
 
 # ---------------------------------------------------------
+# Compute ECDF: x, y
+x, y = ecdf(n_defaults)
 
+# Plot the ECDF with labeled axes
+plt.plot(x, y, marker = '.', linestyle = 'none')
+plt.xlabel('x')
+plt.ylabel('y')
+
+# Show the plot
+plt.show()
+
+# Compute the number of 100-loan simulations with 10 or more defaults: n_lose_money
+n_lose_money = np.sum(n_defaults >= 10)
+
+# Compute and print probability of losing money
+print('Probability of losing money =', n_lose_money / len(n_defaults))
 
 # ---------------------------------------------------------
+# Take 10,000 samples out of the binomial distribution: n_defaults
+n_defaults = np.random.binomial(n=100, p=0.05, size = 10000)
 
+# Compute CDF: x, y
+x, y = ecdf(n_defaults)
+
+# Plot the CDF with axis labels
+_ = plt.plot(x, y, marker = '.', linestyle = 'none')
+_ = plt.xlabel('successes')
+_ = plt.ylabel('cdf')
+
+# Show the plot
+plt.show()
 
 # ---------------------------------------------------------
+# Compute bin edges: bins
+bins = np.arange(0, max(n_defaults) + 1.5) - 0.5
+
+# Generate histogram
+_ = plt.hist(n_defaults, normed=True, bins=bins)
+
+# Label axes
+_ = plt.xlabel('bins')
+_ = plt.ylabel('pmf')
+
+# Show the plot
+plt.show()
+
+# ---------------------------------------------------------
+# Draw 10,000 samples out of Poisson distribution: samples_poisson
+samples_poisson = np.random.poisson(10, 10000)
+
+# Print the mean and standard deviation
+print('Poisson:     ', np.mean(samples_poisson),
+                       np.std(samples_poisson))
+
+# Specify values of n and p to consider for Binomial: n, p
+n = [20, 100, 1000]
+p = [0.5, 0.1, 0.01]
+
+# Draw 10,000 samples for each n,p pair: samples_binomial
+for i in range(3):
+    samples_binomial = np.random.binomial(n[i], p[i], 10000)
+
+    # Print results
+    print('n =', n[i], 'Binom:', np.mean(samples_binomial),
+                                 np.std(samples_binomial))
+
+# ---------------------------------------------------------
+# Draw 10,000 samples out of Poisson distribution: n_nohitters
+n_nohitters = np.random.poisson(251/115, 10000)
+
+# Compute number of samples that are seven or greater: n_large
+n_large = np.sum(n_nohitters >= 7)
+
+# Compute probability of getting seven or more: p_large
+p_large = n_large/10000
+
+# Print the result
+print('Probability of seven or more no-hitters:', p_large)
+
+# ---------------------------------------------------------
+# Draw 100000 samples from Normal distribution with stds of interest: samples_std1, samples_std3, samples_std10
+samples_std1 = np.random.normal(20, 1, size = 100000)
+samples_std3 = np.random.normal(20, 3, size = 100000)
+samples_std10 = np.random.normal(20, 10, size = 100000)
+
+# Make histograms
+_ = plt.hist(samples_std1, histtype = 'step', normed=True, bins=100)
+_ = plt.hist(samples_std3, histtype = 'step', normed=True, bins=100)
+_ = plt.hist(samples_std10, histtype = 'step', normed=True, bins=100)
+
+# Make a legend, set limits and show plot
+_ = plt.legend(('std = 1', 'std = 3', 'std = 10'))
+plt.ylim(-0.01, 0.42)
+plt.show()
+
+# ---------------------------------------------------------
+# Generate CDFs
+x_std1, y_std1 = ecdf(samples_std1)
+x_std3, y_std3 = ecdf(samples_std3)
+x_std10, y_std10 = ecdf(samples_std10)
+
+# Plot CDFs
+_ = plt.plot(x_std1, y_std1, marker = '.', linestyle = 'none')
+_ = plt.plot(x_std3, y_std3, marker = '.', linestyle = 'none')
+_ = plt.plot(x_std10, y_std10, marker = '.', linestyle = 'none')
+
+# Make a legend and show the plot
+_ = plt.legend(('std = 1', 'std = 3', 'std = 10'), loc='lower right')
+plt.show()
+
+# ---------------------------------------------------------
+# Compute mean and standard deviation: mu, sigma
+mu = np.mean(belmont_no_outliers)
+sigma = np.std(belmont_no_outliers)
+
+# Sample out of a normal distribution with this mu and sigma: samples
+samples = np.random.normal(mu, sigma, 10000)
+
+# Get the CDF of the samples and of the data
+x_theor, y_theor = ecdf(samples)
+x, y = ecdf(belmont_no_outliers)
+
+
+# Plot the CDFs and show the plot
+_ = plt.plot(x_theor, y_theor)
+_ = plt.plot(x, y, marker='.', linestyle='none')
+_ = plt.xlabel('Belmont winning time (sec.)')
+_ = plt.ylabel('CDF')
+plt.show()
+
+# ---------------------------------------------------------
+# Take a million samples out of the Normal distribution: samples
+samples = np.random.normal(mu, sigma, 1000000)
+
+# Compute the fraction that are faster than 144 seconds: prob
+prob = np.sum(samples<=144)/1000000
+
+# Print the result
+print('Probability of besting Secretariat:', prob)
+
+# ---------------------------------------------------------
+def successive_poisson(tau1, tau2, size=1):
+    """Compute time for arrival of 2 successive Poisson processes."""
+    # Draw samples out of first exponential distribution: t1
+    t1 = np.random.exponential(tau1, size)
+
+    # Draw samples out of second exponential distribution: t2
+    t2 = np.random.exponential(tau2, size)
+
+    return t1 + t2
+
+# ---------------------------------------------------------
+# Draw samples of waiting times: waiting_times
+waiting_times = successive_poisson(764, 715, 100000)
+
+# Make the histogram
+_ = plt.hist(waiting_times, bins=100, normed=True, histtype='step')
+
+
+# Label axes
+_ = plt.xlabel('time')
+_ = plt.ylabel('prob')
+
+# Show the plot
+plt.show()
 
 
 # ---------------------------------------------------------
