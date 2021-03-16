4eb518e0b3530e0c060efc0d009edea5d69cb01f
gvicari <indirizzoemail@gmail.com>
2021-03-16 19:16:36 +0000

    Completed Module 10

--- Module10.py
+++ Module10.py
@@ -316,119 +316,312 @@ plt.title('ROC Curve')
 plt.show()
 
 # ---------------------------------------------------------
+# Import necessary modules
+from sklearn.metrics import roc_auc_score
+from sklearn.model_selection import cross_val_score
 
+# Compute predicted probabilities: y_pred_prob
+y_pred_prob = logreg.predict_proba(X_test)[:,1]
 
-# ---------------------------------------------------------
+# Compute and print AUC score
+print("AUC: {}".format(roc_auc_score(y_test, y_pred_prob)))
 
+# Compute cross-validated AUC scores: cv_auc
+cv_auc = cross_val_score(logreg, X, y, scoring = 'roc_auc', cv = 5)
 
-# ---------------------------------------------------------
-
-
-# ---------------------------------------------------------
-
-
-# ---------------------------------------------------------
-
-
-# ---------------------------------------------------------
-
-
-# ---------------------------------------------------------
-
+# Print list of AUC scores
+print("AUC scores computed using 5-fold cross-validation: {}".format(cv_auc))
 
 # ---------------------------------------------------------
+# Import necessary modules
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import GridSearchCV
 
+# Setup the hyperparameter grid
+c_space = np.logspace(-5, 8, 15)
+param_grid = {'C': c_space}
 
-# ---------------------------------------------------------
+# Instantiate a logistic regression classifier: logreg
+logreg = LogisticRegression()
 
+# Instantiate the GridSearchCV object: logreg_cv
+logreg_cv = GridSearchCV(logreg, param_grid, cv=5)
 
-# ---------------------------------------------------------
+# Fit it to the data
+logreg_cv.fit(X, y)
 
-
-# ---------------------------------------------------------
-
-
-# ---------------------------------------------------------
-
-
-# ---------------------------------------------------------
-
-
-# ---------------------------------------------------------
-
-
-# ---------------------------------------------------------
-
+# Print the tuned parameters and score
+print("Tuned Logistic Regression Parameters: {}".format(logreg_cv.best_params_))
+print("Best score is {}".format(logreg_cv.best_score_))
 
 # ---------------------------------------------------------
+# Import necessary modules
+from scipy.stats import randint
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import RandomizedSearchCV
 
+# Setup the parameters and distributions to sample from: param_dist
+param_dist = {"max_depth": [3, None],
+              "max_features": randint(1, 9),
+              "min_samples_leaf": randint(1, 9),
+              "criterion": ["gini", "entropy"]}
 
-# ---------------------------------------------------------
+# Instantiate a Decision Tree classifier: tree
+tree = DecisionTreeClassifier()
 
+# Instantiate the RandomizedSearchCV object: tree_cv
+tree_cv = RandomizedSearchCV(tree, param_dist, cv=5)
 
-# ---------------------------------------------------------
+# Fit it to the data
+tree_cv.fit(X, y)
 
-
-# ---------------------------------------------------------
-
-
-# ---------------------------------------------------------
-
-
-# ---------------------------------------------------------
-
-
-# ---------------------------------------------------------
-
-
-# ---------------------------------------------------------
-
-
-# ---------------------------------------------------------
-
+# Print the tuned parameters and score
+print("Tuned Decision Tree Parameters: {}".format(tree_cv.best_params_))
+print("Best score is {}".format(tree_cv.best_score_))
 
 # ---------------------------------------------------------
+# Import necessary modules
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import GridSearchCV
 
+# Create the hyperparameter grid
+c_space = np.logspace(-5, 8, 15)
+param_grid = {'C': c_space, 'penalty': ['l1', 'l2']}
 
-# ---------------------------------------------------------
+# Instantiate the logistic regression classifier: logreg
+logreg = LogisticRegression()
 
+# Create train and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.4, random_state = 42)
 
-# ---------------------------------------------------------
+# Instantiate the GridSearchCV object: logreg_cv
+logreg_cv = GridSearchCV(logreg, param_grid, cv = 5)
 
+# Fit it to the training data
+logreg_cv.fit(X_train, y_train)
 
-# ---------------------------------------------------------
-
-
-# ---------------------------------------------------------
-
-
-# ---------------------------------------------------------
-
-
-# ---------------------------------------------------------
-
-
-# ---------------------------------------------------------
-
-
-# ---------------------------------------------------------
-
-
-# ---------------------------------------------------------
-
+# Print the optimal parameters and best score
+print("Tuned Logistic Regression Parameter: {}".format(logreg_cv.best_params_))
+print("Tuned Logistic Regression Accuracy: {}".format(logreg_cv.best_score_))
 
 # ---------------------------------------------------------
+# Import necessary modules
+from sklearn.linear_model import ElasticNet
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import GridSearchCV, train_test_split
 
+# Create train and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.4, random_state=42)
+
+# Create the hyperparameter grid
+l1_space = np.linspace(0, 1, 30)
+param_grid = {'l1_ratio': l1_space}
+
+# Instantiate the ElasticNet regressor: elastic_net
+elastic_net = ElasticNet()
+
+# Setup the GridSearchCV object: gm_cv
+gm_cv = GridSearchCV(elastic_net, param_grid, cv=5)
+
+# Fit it to the training data
+gm_cv.fit(X_train, y_train)
+
+# Predict on the test set and compute metrics
+y_pred = gm_cv.predict(X_test)
+r2 = gm_cv.score(X_test, y_test)
+mse = mean_squared_error(y_test, y_pred)
+print("Tuned ElasticNet l1 ratio: {}".format(gm_cv.best_params_))
+print("Tuned ElasticNet R squared: {}".format(r2))
+print("Tuned ElasticNet MSE: {}".format(mse))
 
 # ---------------------------------------------------------
+# Import pandas
+import pandas as pd
 
+# Read 'gapminder.csv' into a DataFrame: df
+df = pd.read_csv('gapminder.csv')
+
+# Create a boxplot of life expectancy per region
+df.boxplot('life', 'Region', rot=60)
+
+# Show the plot
+plt.show()
 
 # ---------------------------------------------------------
+# Create dummy variables: df_region
+df_region = pd.get_dummies(df)
 
+# Print the columns of df_region
+print(df_region.columns)
+
+# Create dummy variables with drop_first=True: df_region
+df_region = pd.get_dummies(df, drop_first = True)
+
+# Print the new columns of df_region
+print(df_region.columns)
 
 # ---------------------------------------------------------
+# Import necessary modules
+from sklearn.linear_model import Ridge
+from sklearn.model_selection import cross_val_score
 
+# Instantiate a ridge regressor: ridge
+ridge = Ridge(alpha=0.5, normalize=True)
+
+# Perform 5-fold cross-validation: ridge_cv
+ridge_cv = cross_val_score(ridge, X, y, cv = 5)
+
+# Print the cross-validated scores
+print(ridge_cv)
 
 # ---------------------------------------------------------
+# Convert '?' to NaN
+df[df == '?'] = np.nan
 
+# Print the number of NaNs
+print(df.isnull().sum())
+
+# Print shape of original DataFrame
+print("Shape of Original DataFrame: {}".format(df.shape))
+
+# Drop missing values and print shape of new DataFrame
+df = df.dropna()
+
+# Print shape of new DataFrame
+print("Shape of DataFrame After Dropping All Rows with Missing Values: {}".format(df.shape))
+
+# ---------------------------------------------------------
+# Import the Imputer module
+from sklearn.preprocessing import Imputer
+from sklearn.svm import SVC
+
+# Setup the Imputation transformer: imp
+imp = Imputer(missing_values='NaN', strategy='most_frequent', axis=0)
+
+# Instantiate the SVC classifier: clf
+clf = SVC()
+
+# Setup the pipeline with the required steps: steps
+steps = [('imputation', imp),
+        ('SVM', clf)]
+
+# ---------------------------------------------------------
+# Import necessary modules
+from sklearn.preprocessing import Imputer
+from sklearn.pipeline import Pipeline
+from sklearn.svm import SVC
+
+# Setup the pipeline steps: steps
+steps = [('imputation', Imputer(missing_values='NaN', strategy='most_frequent', axis=0)),
+        ('SVM', SVC())]
+
+# Create the pipeline: pipeline
+pipeline = Pipeline(steps)
+
+# Create training and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+# Fit the pipeline to the train set
+pipeline.fit(X_train, y_train)
+
+# Predict the labels of the test set
+y_pred = pipeline.predict(X_test)
+
+# Compute metrics
+print(classification_report(y_test, y_pred))
+
+# ---------------------------------------------------------
+# Import scale
+from sklearn.preprocessing import scale
+
+# Scale the features: X_scaled
+X_scaled = scale(X)
+
+# Print the mean and standard deviation of the unscaled features
+print("Mean of Unscaled Features: {}".format(np.mean(X)))
+print("Standard Deviation of Unscaled Features: {}".format(np.std(X)))
+
+# Print the mean and standard deviation of the scaled features
+print("Mean of Scaled Features: {}".format(np.mean(X_scaled)))
+print("Standard Deviation of Scaled Features: {}".format(np.std(X_scaled)))
+
+# ---------------------------------------------------------
+# Import the necessary modules
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
+
+# Setup the pipeline steps: steps
+steps = [('scaler', StandardScaler()),
+         ('knn', KNeighborsClassifier())]
+
+# Create the pipeline: pipeline
+pipeline = Pipeline(steps)
+
+# Create train and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+# Fit the pipeline to the training set: knn_scaled
+knn_scaled = pipeline.fit(X_train, y_train)
+
+# Instantiate and fit a k-NN classifier to the unscaled data
+knn_unscaled = KNeighborsClassifier().fit(X_train, y_train)
+
+# Compute and print metrics
+print('Accuracy with Scaling: {}'.format(knn_scaled.score(X_test, y_test)))
+print('Accuracy without Scaling: {}'.format(knn_unscaled.score(X_test, y_test)))
+
+# ---------------------------------------------------------
+# Setup the pipeline
+steps = [('scaler', StandardScaler()),
+         ('SVM', SVC())]
+
+pipeline = Pipeline(steps)
+
+# Specify the hyperparameter space
+parameters = {'SVM__C':[1, 10, 100],
+              'SVM__gamma':[0.1, 0.01]}
+
+# Create train and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state=21)
+
+# Instantiate the GridSearchCV object: cv
+cv = GridSearchCV(pipeline, parameters)
+
+# Fit to the training set
+cv.fit(X_train, y_train)
+
+# Predict the labels of the test set: y_pred
+y_pred = cv.predict(X_test)
+
+# Compute and print metrics
+print("Accuracy: {}".format(cv.score(X_test, y_test)))
+print(classification_report(y_test, y_pred))
+print("Tuned Model Parameters: {}".format(cv.best_params_))
+
+# ---------------------------------------------------------
+# Setup the pipeline steps: steps
+steps = [('imputation', Imputer(missing_values='NaN', strategy='mean', axis=0)),
+         ('scaler', StandardScaler()),
+         ('elasticnet', ElasticNet())]
+
+# Create the pipeline: pipeline
+pipeline = Pipeline(steps)
+
+# Specify the hyperparameter space
+parameters = {'elasticnet__l1_ratio':np.linspace(0,1,30)}
+
+# Create train and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=42)
+
+# Create the GridSearchCV object: gm_cv
+gm_cv = GridSearchCV(pipeline, parameters)
+
+# Fit to the training set
+gm_cv.fit(X_train, y_train)
+
+# Compute and print the metrics
+r2 = gm_cv.score(X_test, y_test)
+print("Tuned ElasticNet Alpha: {}".format(gm_cv.best_params_))
+print("Tuned ElasticNet R squared: {}".format(r2))
 
