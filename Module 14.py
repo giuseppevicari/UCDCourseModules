b42cc0c71616f442a80d08cd17e3160db04c9de4
gvicari <indirizzoemail@gmail.com>
2021-04-12 21:23:10 +0100

    Started Module 14

--- Module 14.py
+++ Module 14.py
@@ -1,11 +1,40 @@
 # ---------------------------------------------------------
+# Import plotting class from matplotlib library
+from matplotlib import pyplot as plt
 
+# Create a scatter plot
+plt.scatter(x, y)
+
+# Display the scatter plot
+plt.show()
 
 # ---------------------------------------------------------
+# Import linkage and fcluster functions
+from scipy.cluster.hierarchy import linkage, fcluster
 
+# Use the linkage() function to compute distance
+Z = linkage(df, 'ward')
+
+# Generate cluster labels
+df['cluster_labels'] = fcluster(Z, 2, criterion='maxclust')
+
+# Plot the points with seaborn
+sns.scatterplot(x='x', y='y', hue='cluster_labels', data=df)
+plt.show()
 
 # ---------------------------------------------------------
+# Import kmeans and vq functions
+from scipy.cluster.vq import kmeans, vq
 
+# Compute cluster centers
+centroids,_ = kmeans(df, 2)
+
+# Assign cluster labels
+df['cluster_labels'], _ = vq(df, centroids)
+
+# Plot the points with seaborn
+sns.scatterplot(x='x', y='y', hue='cluster_labels', data=df)
+plt.show()
 
 # ---------------------------------------------------------
 
