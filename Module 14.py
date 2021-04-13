4676fa86475fa592d35b2c4f1f7b5827415cf844
gvicari <indirizzoemail@gmail.com>
2021-04-13 20:36:47 +0100

    Updated Module 14

--- Module 14.py
+++ Module 14.py
@@ -37,19 +37,64 @@ sns.scatterplot(x='x', y='y', hue='cluster_labels', data=df)
 plt.show()
 
 # ---------------------------------------------------------
+# Import the whiten function
+from scipy.cluster.vq import whiten
 
+goals_for = [4,3,2,3,1,1,2,0,1,4]
 
-# ---------------------------------------------------------
-
-
-# ---------------------------------------------------------
-
-
-# ---------------------------------------------------------
-
+# Use the whiten() function to standardize the data
+scaled_data = whiten(goals_for)
+print(scaled_data)
 
 # ---------------------------------------------------------
+# Plot original data
+plt.plot(goals_for, label='original')
 
+# Plot scaled data
+plt.plot(scaled_data, label='scaled')
+
+# Show the legend in the plot
+plt.legend()
+
+# Display the plot
+plt.show()
+
+# ---------------------------------------------------------
+# Prepare data
+rate_cuts = [0.0025, 0.001, -0.0005, -0.001, -0.0005, 0.0025, -0.001, -0.0015, -0.001, 0.0005]
+
+# Use the whiten() function to standardize the data
+scaled_data = whiten(rate_cuts)
+
+# Plot original data
+plt.plot(rate_cuts, label='original')
+
+# Plot scaled data
+plt.plot(scaled_data, label='scaled')
+
+plt.legend()
+plt.show()
+
+# ---------------------------------------------------------
+# Scale wage and value
+fifa['scaled_wage'] = whiten(fifa['eur_wage'])
+fifa['scaled_value'] = whiten(fifa['eur_value'])
+
+# Plot the two columns in a scatter plot
+fifa.plot(x='scaled_wage', y='scaled_value', kind='scatter')
+plt.show()
+
+# ---------------------------------------------------------
+# Scale wage and value
+fifa['scaled_wage'] = whiten(fifa['eur_wage'])
+fifa['scaled_value'] = whiten(fifa['eur_value'])
+
+# Plot the two columns in a scatter plot
+fifa.plot(x='scaled_wage', y='scaled_value', kind = 'scatter')
+plt.show()
+
+# Check mean and standard deviation of scaled values
+print(fifa[['scaled_wage', 'scaled_value']].describe())
 
 # ---------------------------------------------------------
 
