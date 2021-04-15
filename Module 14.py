9ae03a51df14718ef2028a6ab3ee34d2d7ec4e00
gvicari <indirizzoemail@gmail.com>
2021-04-15 10:27:28 +0100

    Updated Module 14

--- Module 14.py
+++ Module 14.py
@@ -97,28 +97,100 @@ plt.show()
 print(fifa[['scaled_wage', 'scaled_value']].describe())
 
 # ---------------------------------------------------------
+# Import the fcluster and linkage functions
+from scipy.cluster.hierarchy import fcluster, linkage
 
+# Use the linkage() function
+distance_matrix = linkage(comic_con[['x_scaled', 'y_scaled']], method = 'ward', metric = 'euclidean')
 
-# ---------------------------------------------------------
+# Assign cluster labels
+comic_con['cluster_labels'] = fcluster(distance_matrix, 2, criterion='maxclust')
 
-
-# ---------------------------------------------------------
-
-
-# ---------------------------------------------------------
-
-
-# ---------------------------------------------------------
-
-
-# ---------------------------------------------------------
-
+# Plot clusters
+sns.scatterplot(x='x_scaled', y='y_scaled',
+                hue='cluster_labels', data = comic_con)
+plt.show()
 
 # ---------------------------------------------------------
+# Import the fcluster and linkage functions
+from scipy.cluster.hierarchy import linkage, fcluster
 
+# Use the linkage() function
+distance_matrix = linkage(comic_con[['x_scaled', 'y_scaled']], method = 'single', metric = 'euclidean')
+
+# Assign cluster labels
+comic_con['cluster_labels'] = fcluster(distance_matrix, 2, criterion='maxclust')
+
+# Plot clusters
+sns.scatterplot(x='x_scaled', y='y_scaled',
+                hue='cluster_labels', data = comic_con)
+plt.show()
 
 # ---------------------------------------------------------
+# Import the fcluster and linkage functions
+from scipy.cluster.hierarchy import linkage, fcluster
 
+# Use the linkage() function
+distance_matrix = linkage(comic_con[['x_scaled', 'y_scaled']], method='complete', metric='euclidean')
+
+# Assign cluster labels
+comic_con['cluster_labels'] = fcluster(distance_matrix, 2, criterion='maxclust')
+
+# Plot clusters
+sns.scatterplot(x='x_scaled', y='y_scaled',
+                hue='cluster_labels', data = comic_con)
+plt.show()
+
+# ---------------------------------------------------------
+# Import the pyplot class
+from matplotlib import pyplot as plt
+
+# Define a colors dictionary for clusters
+colors = {1:'red', 2:'blue'}
+
+# Plot a scatter plot
+comic_con.plot.scatter(x='x_scaled',
+                	   y='y_scaled',
+                	   c=comic_con['cluster_labels'].apply(lambda x: colors[x]))
+plt.show()
+
+# ---------------------------------------------------------
+# Import the seaborn module
+import seaborn as sns
+
+# Plot a scatter plot using seaborn
+sns.scatterplot(x='x_scaled',
+                y='y_scaled',
+                hue='cluster_labels',
+                data = comic_con)
+plt.show()
+
+# ---------------------------------------------------------
+# Import the dendrogram function
+from scipy.cluster.hierarchy import dendrogram
+
+# Create a dendrogram
+dn = dendrogram(distance_matrix)
+
+# Display the dendogram
+plt.show()
+
+# ---------------------------------------------------------
+%timeit linkage(comic_con[['x_scaled', 'y_scaled']], method='ward', metric='euclidean')
+
+# ---------------------------------------------------------
+# Fit the data into a hierarchical clustering algorithm
+distance_matrix = linkage(fifa[['scaled_sliding_tackle', 'scaled_aggression']], 'ward')
+
+# Assign cluster labels to each row of data
+fifa['cluster_labels'] = fcluster(distance_matrix, 3, criterion='maxclust')
+
+# Display cluster centers of each cluster
+print(fifa[['scaled_sliding_tackle', 'scaled_aggression', 'cluster_labels']].groupby('cluster_labels').mean())
+
+# Create a scatter plot through seaborn
+sns.scatterplot(x='scaled_sliding_tackle', y='scaled_aggression', hue='cluster_labels', data=fifa)
+plt.show()
 
 # ---------------------------------------------------------
 
